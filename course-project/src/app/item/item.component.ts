b716f1eff01fb222f82beb0fb766d77b696a18fb
anthony-deleon1214 <anthony.deleon1214@gmail.com>
2023-02-06 11:33:27 -0500

    Adding star-wars service and refactoring around it

--- course-project/src/app/item/item.component.ts
+++ course-project/src/app/item/item.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { StarWarsService } from '../star-wars.service';
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
-  styleUrls: ['./item.component.css']
+  styleUrls: ['./item.component.css'],
 })
 export class ItemComponent {
+  @Input() character;
+  swService: StarWarsService;
 
+  constructor(swService: StarWarsService) {
+    this.swService = swService;
+  }
+
+  onAssign(side) {
+    this.swService.onSideChosen({ name: this.character.name, side: side })
+  };
 }
--- course-project/src/app/tabs/tabs.component.ts
+++ course-project/src/app/tabs/tabs.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { StarWarsService } from '../star-wars.service';
 
 @Component({
   selector: 'app-tabs',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./tabs.component.css']
 })
 export class TabsComponent {
+  characters = [];
+  chosenList = 'all';
+  swService: StarWarsService;
 
+  constructor(swService: StarWarsService) {
+    this.swService = swService;
+  };
+
+  onChoose(side) {
+    this.chosenList = side;
+  };
+
+  getCharacters() {
+    this.characters = this.swService.getCharacters(this.chosenList);
+    return this.characters;
+  }
+
+  onSideChosen(charInfo) {
+    const pos = this.characters.findIndex((char) => {
+      return char.name === charInfo.name;
+    })
+    this.characters[pos].side = charInfo.side;
+  }
 }
